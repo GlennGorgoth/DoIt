d1c9ab8b3ef2a40a93c23deefb1fbb7c7d7d0787
Glenn Gorgoth <arnoldfilm@gmail.com>
2017-06-09 10:49:04 -0600

    Initial Commit

---
+++ Do It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do It
+//
+//  Created by Brandon Arnold on 6/9/17.
+//  Copyright © 2017 Brandon Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
