516a2453045c1c4b9e0827b96a6eb46cb34a29bd
Glenn Gorgoth <arnoldfilm@gmail.com>
2017-06-09 12:18:40 -0600

    New Screen

---
+++ Do It/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  Do It
+//
+//  Created by Brandon Arnold on 6/9/17.
+//  Copyright © 2017 Brandon Arnold. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
